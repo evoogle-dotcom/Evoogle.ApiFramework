7fc52857e2c6c12c178edf1f72dc727c3fba87d7
Scott McDonald <scott-mcdonald@users.noreply.github.com>
2025-06-11 01:22:24 -0400

    Replace ApiSchema with named type version

--- Source/Evoogle.ApiFramework.Core/Schema/ApiSchema.cs
+++ Source/Evoogle.ApiFramework.Core/Schema/ApiSchema.cs
@@ -9,19 +9,36 @@ using Evoogle.Extension;
 namespace Evoogle.ApiFramework.Schema;
 
 /// <summary>
-///     Represents a collection of <see cref="ApiType"/> instances making up a schema.
+///     Represents a collection of <see cref="ApiNamedType"/> instances making up a schema.
 /// </summary>
 public sealed class ApiSchema : ExtensibleBase
 {
     #region ApiSchema Fields
-    private readonly Dictionary<string, ApiType> _apiNameLookup;
-    private readonly Dictionary<string, ApiType> _clrNameLookup;
-    private readonly Dictionary<Type, ApiType> _clrTypeLookup;
+    private readonly Dictionary<string, ApiNamedType> _apiNameLookup;
+    private readonly Dictionary<Type, ApiNamedType> _clrTypeLookup;
+
+    private readonly Dictionary<string, ApiEnumType> _enumApiNameLookup;
+    private readonly Dictionary<Type, ApiEnumType> _enumClrTypeLookup;
+
+    private readonly Dictionary<string, ApiObjectType> _objectApiNameLookup;
+    private readonly Dictionary<Type, ApiObjectType> _objectClrTypeLookup;
+
+    private readonly Dictionary<string, ApiScalarType> _scalarApiNameLookup;
+    private readonly Dictionary<Type, ApiScalarType> _scalarClrTypeLookup;
     #endregion
 
     #region ApiSchema Properties
-    /// <summary>Gets all API types contained within this schema.</summary>
-    public IReadOnlyCollection<ApiType> ApiTypes { get; }
+    /// <summary>Gets all API named types contained within this schema.</summary>
+    public IReadOnlyCollection<ApiNamedType> ApiTypes { get; }
+
+    /// <summary>Gets all API enumeration types contained within this schema.</summary>
+    public IReadOnlyCollection<ApiEnumType> ApiEnumerationTypes { get; }
+
+    /// <summary>Gets all API object types contained within this schema.</summary>
+    public IReadOnlyCollection<ApiObjectType> ApiObjectTypes { get; }
+
+    /// <summary>Gets all API scalar types contained within this schema.</summary>
+    public IReadOnlyCollection<ApiScalarType> ApiScalarTypes { get; }
     #endregion
 
     #region Constructors
@@ -34,49 +51,66 @@ public sealed class ApiSchema : ExtensibleBase
     public ApiSchema(IEnumerable<ApiType> apiTypes)
     {
         var values = apiTypes.SafeToArray();
+        var namedValues = values.OfType<ApiNamedType>().ToArray();
 
-        ValidateUnique(values.OfType<ApiNamedType>(), x => x.ApiName, nameof(ApiNamedType.ApiName));
-        ValidateUnique(values, x => x.ClrType.Name, "ClrName");
-        ValidateUnique(values, x => x.ClrType, nameof(ApiType.ClrType));
+        ValidateUnique(namedValues, x => x.ApiName, nameof(ApiNamedType.ApiName));
+        ValidateUnique(namedValues, x => x.ClrType.Name, "ClrName");
+        ValidateUnique(namedValues, x => x.ClrType, nameof(ApiType.ClrType));
 
-        this.ApiTypes = values;
+        this.ApiTypes = namedValues;
+        this.ApiEnumerationTypes = namedValues.OfType<ApiEnumType>().ToArray();
+        this.ApiObjectTypes = namedValues.OfType<ApiObjectType>().ToArray();
+        this.ApiScalarTypes = namedValues.OfType<ApiScalarType>().ToArray();
 
-        this._apiNameLookup = values.OfType<ApiNamedType>().Cast<ApiType>().ToDictionary(x => ((ApiNamedType)x).ApiName, StringComparer.OrdinalIgnoreCase);
-        this._clrNameLookup = values.ToDictionary(x => x.ClrType.Name, StringComparer.OrdinalIgnoreCase);
-        this._clrTypeLookup = values.ToDictionary(x => x.ClrType);
+        this._apiNameLookup = namedValues.ToDictionary(x => x.ApiName, StringComparer.OrdinalIgnoreCase);
+        this._clrTypeLookup = namedValues.ToDictionary(x => x.ClrType);
+
+        this._enumApiNameLookup = this.ApiEnumerationTypes.ToDictionary(x => x.ApiName, StringComparer.OrdinalIgnoreCase);
+        this._enumClrTypeLookup = this.ApiEnumerationTypes.ToDictionary(x => x.ClrType);
+
+        this._objectApiNameLookup = this.ApiObjectTypes.ToDictionary(x => x.ApiName, StringComparer.OrdinalIgnoreCase);
+        this._objectClrTypeLookup = this.ApiObjectTypes.ToDictionary(x => x.ClrType);
+
+        this._scalarApiNameLookup = this.ApiScalarTypes.ToDictionary(x => x.ApiName, StringComparer.OrdinalIgnoreCase);
+        this._scalarClrTypeLookup = this.ApiScalarTypes.ToDictionary(x => x.ClrType);
     }
     #endregion
 
     #region ApiSchema Methods
-    /// <summary>
-    ///     Attempts to retrieve an <see cref="ApiType"/> by its API name.
-    /// </summary>
-    /// <param name="apiName">The API name of the type to retrieve.</param>
-    /// <param name="apiType">When this method returns, contains the <see cref="ApiType"/> if found.</param>
-    /// <returns>True if the type is found; otherwise, false.</returns>
-    public bool TryGetByApiName(string apiName, out ApiType? apiType)
-        => this._apiNameLookup.TryGetValue(apiName, out apiType);
+    /// <summary>Attempts to retrieve an API named type by its API name.</summary>
+    public bool TryGetApiType(string apiName, out ApiNamedType? apiNamedType)
+        => this._apiNameLookup.TryGetValue(apiName, out apiNamedType);
 
-    /// <summary>
-    ///     Attempts to retrieve an <see cref="ApiType"/> by its CLR name.
-    /// </summary>
-    /// <param name="clrName">The CLR type name to look up.</param>
-    /// <param name="apiType">When this method returns, contains the <see cref="ApiType"/> if found.</param>
-    /// <returns>True if the type is found; otherwise, false.</returns>
-    public bool TryGetByClrName(string clrName, out ApiType? apiType)
-        => this._clrNameLookup.TryGetValue(clrName, out apiType);
+    /// <summary>Attempts to retrieve an API named type by its CLR type.</summary>
+    public bool TryGetApiType(Type clrType, out ApiNamedType? apiNamedType)
+        => this._clrTypeLookup.TryGetValue(clrType, out apiNamedType);
 
-    /// <summary>
-    ///     Attempts to retrieve an <see cref="ApiType"/> by its CLR type.
-    /// </summary>
-    /// <param name="clrType">The CLR <see cref="Type"/> to look up.</param>
-    /// <param name="apiType">When this method returns, contains the <see cref="ApiType"/> if found.</param>
-    /// <returns>True if the type is found; otherwise, false.</returns>
-    public bool TryGetByClrType(Type clrType, out ApiType? apiType)
-        => this._clrTypeLookup.TryGetValue(clrType, out apiType);
+    /// <summary>Attempts to retrieve an API enumeration type by its API name.</summary>
+    public bool TryGetApiEnumerationType(string apiName, out ApiEnumType? apiEnumerationType)
+        => this._enumApiNameLookup.TryGetValue(apiName, out apiEnumerationType);
+
+    /// <summary>Attempts to retrieve an API enumeration type by its CLR type.</summary>
+    public bool TryGetApiEnumerationType(Type clrType, out ApiEnumType? apiEnumerationType)
+        => this._enumClrTypeLookup.TryGetValue(clrType, out apiEnumerationType);
+
+    /// <summary>Attempts to retrieve an API object type by its API name.</summary>
+    public bool TryGetApiObjectType(string apiName, out ApiObjectType? apiObjectType)
+        => this._objectApiNameLookup.TryGetValue(apiName, out apiObjectType);
+
+    /// <summary>Attempts to retrieve an API object type by its CLR type.</summary>
+    public bool TryGetApiObjectType(Type clrType, out ApiObjectType? apiObjectType)
+        => this._objectClrTypeLookup.TryGetValue(clrType, out apiObjectType);
+
+    /// <summary>Attempts to retrieve an API scalar type by its API name.</summary>
+    public bool TryGetApiScalarType(string apiName, out ApiScalarType? apiScalarType)
+        => this._scalarApiNameLookup.TryGetValue(apiName, out apiScalarType);
+
+    /// <summary>Attempts to retrieve an API scalar type by its CLR type.</summary>
+    public bool TryGetApiScalarType(Type clrType, out ApiScalarType? apiScalarType)
+        => this._scalarClrTypeLookup.TryGetValue(clrType, out apiScalarType);
 
     private static void ValidateUnique<TApiType, TKey>(IEnumerable<TApiType> values, Func<TApiType, TKey> keySelector, string propertyName)
-        where TApiType : ApiType
+        where TApiType : ApiNamedType
         where TKey : notnull
     {
         var duplicates = values
