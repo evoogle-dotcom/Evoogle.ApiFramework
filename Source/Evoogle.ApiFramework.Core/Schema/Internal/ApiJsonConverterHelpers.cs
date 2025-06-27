9f0169148d9086171e1ec2aad56974862d032d4f
Scott McDonald <scott@evoogle.com>
2025-06-27 01:25:16 -0400

    dotnet format changes
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Source/Evoogle.ApiFramework.Core/Schema/Internal/ApiJsonConverterHelpers.cs
+++ Source/Evoogle.ApiFramework.Core/Schema/Internal/ApiJsonConverterHelpers.cs
@@ -1,17 +1,15 @@
-// Copyright (c) 2024-2025 Evoogle.com
+﻿// Copyright (c) 2024-2025 Evoogle.com
 // SPDX-License-Identifier: MIT
 //
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
-
-using Evoogle.Json;
 using Evoogle.Extension;
-using Evoogle.Logging;
-
-using Microsoft.Extensions.Logging;
 using Evoogle.Extensions;
+using Evoogle.Json;
+using Evoogle.Logging;
+using Microsoft.Extensions.Logging;
 
 namespace Evoogle.ApiFramework.Schema.Internal;
 
