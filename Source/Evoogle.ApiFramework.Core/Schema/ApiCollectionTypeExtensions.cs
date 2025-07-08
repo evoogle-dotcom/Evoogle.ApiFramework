87813c4b74d117846f43223767892a793c2a928f
Scott McDonald <scott@evoogle.com>
2025-07-08 14:54:39 -0400

    Add ApiCollectionTypeExtensions and ApiPropertyExtensions
    Signed-off-by: Scott McDonald <scott@evoogle.com>

---
+++ Source/Evoogle.ApiFramework.Core/Schema/ApiCollectionTypeExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2024-2025 Evoogle.com
+// SPDX-License-Identifier: MIT
+//
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+namespace Evoogle.ApiFramework.Schema;
+
+/// <summary>
+///     Extension methods for <see cref="ApiCollectionType"/> class.
+/// </summary>
+public static class ApiCollectionTypeExtensions
+{
+    #region Extension Methods
+    /// <summary>
+    ///     Determines whether the items in the <see cref="ApiCollectionType"/> are required.
+    /// </summary>
+    /// <param name="apiCollectionType">The <see cref="ApiCollectionType"/> instance.</param>
+    /// <returns>
+    ///     <c>true</c> if the <see cref="ApiCollectionType.ApiItemTypeModifiers"/> has the <see cref="ApiTypeModifiers.Required"/> flag set; otherwise, <c>false</c>.
+    /// </returns>    
+    public static bool IsItemRequired(this ApiCollectionType apiCollectionType) => apiCollectionType.ApiItemTypeModifiers.HasFlag(ApiTypeModifiers.Required);
+
+    /// <summary>
+    ///     Determines whether the items in the <see cref="ApiCollectionType"/> are optional.
+    /// </summary>
+    /// <param name="apiCollectionType">The <see cref="ApiCollectionType"/> instance.</param>
+    /// <returns>
+    ///     <c>true</c> if the <see cref="ApiCollectionType.ApiItemTypeModifiers"/> does not have the <see cref="ApiTypeModifiers.Required"/> flag set; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool IsItemOptional(this ApiCollectionType apiCollectionType) => !apiCollectionType.ApiItemTypeModifiers.HasFlag(ApiTypeModifiers.Required);
+    #endregion
+}
---
+++ Source/Evoogle.ApiFramework.Core/Schema/ApiPropertyExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2024-2025 Evoogle.com
+// SPDX-License-Identifier: MIT
+//
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+namespace Evoogle.ApiFramework.Schema;
+
+/// <summary>
+///     Extension methods for <see cref="ApiProperty"/> class.
+/// </summary>
+public static class ApiPropertyExtensions
+{
+    #region Extension Methods
+    /// <summary>
+    ///     Determines whether the <see cref="ApiProperty"/> is marked as required.
+    /// </summary>
+    /// <param name="apiProperty">The <see cref="ApiProperty"/> instance.</param>
+    /// <returns>
+    ///     <c>true</c> if the <see cref="ApiProperty.ApiTypeModifiers"/> has the <see cref="ApiTypeModifiers.Required"/> flag set; otherwise, <c>false</c>.
+    /// </returns>    
+    public static bool IsRequired(this ApiProperty apiProperty) => apiProperty.ApiTypeModifiers.HasFlag(ApiTypeModifiers.Required);
+
+    /// <summary>
+    ///     Determines whether the <see cref="ApiProperty"/> is marked as optional.
+    /// </summary>
+    /// <param name="apiProperty">The <see cref="ApiProperty"/> instance.</param>
+    /// <returns>
+    ///     <c>true</c> if the <see cref="ApiProperty.ApiTypeModifiers"/> does not have the <see cref="ApiTypeModifiers.Required"/> flag set; otherwise, <c>false</c>.
+    /// </returns
+    public static bool IsOptional(this ApiProperty apiProperty) => !apiProperty.ApiTypeModifiers.HasFlag(ApiTypeModifiers.Required);
+    #endregion
+}
