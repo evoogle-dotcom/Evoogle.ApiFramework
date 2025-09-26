18796be8fb035800e6fa87b178a5917743ef4e88
Scott McDonald <scott@evoogle.com>
2025-09-26 19:00:51 -0400

    Clean up ApiSchemaBuilderContextTests.cs
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Tests/Evoogle.ApiFramework.Core.Tests/Schema/Configuration/ApiSchemaBuilderContextTests.cs
+++ Tests/Evoogle.ApiFramework.Core.Tests/Schema/Configuration/ApiSchemaBuilderContextTests.cs
@@ -4,6 +4,7 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
 using Evoogle.ApiFramework.Schema.TestData;
+using Evoogle.Extensions;
 using Evoogle.XUnit;
 
 using FluentAssertions;
@@ -12,76 +13,66 @@ namespace Evoogle.ApiFramework.Schema.Configuration;
 
 public class ApiSchemaBuilderContextTests(ITestOutputHelper output) : XUnitTests(output)
 {
-    #region Test Types
-    public class Parent
-    {
-        public string Name { get; set; } = string.Empty;
-        public List<Parent> Children { get; set; } = [];
-    }
-    #endregion
-
-    public enum BuilderKind
-    {
-        Enum,
-        Object,
-        Scalar
-    }
-
+    #region Test Classes
     public class GetOrAddTest : XUnitTest
     {
-        public BuilderKind? Kind { get; init; }
-        public Type? ClrType { get; init; }
+        #region User Supplied Properties
+        public string MethodName { get; init; } = null!;
+        public Type ClrType { get; init; } = null!;
+        #endregion
 
+        #region Calculated Properties
         private object? Builder1 { get; set; }
         private object? Builder2 { get; set; }
+        #endregion
 
+        #region XUnitTest Methods
         protected override void Arrange()
         {
-            this.WriteLine($"Kind: {this.Kind}");
-            this.WriteLine($"ClrType: {this.ClrType}");
+            this.WriteLine($"MethodName: {this.MethodName.SafeToString()}");
+            this.WriteLine($"ClrType: {this.ClrType.SafeToName()}");
             this.WriteLine();
         }
 
         protected override void Act()
         {
             var context = new ApiSchemaBuilderContext();
-            var methodName = this.Kind switch
-            {
-                BuilderKind.Enum => "GetOrAddEnumTypeBuilder",
-                BuilderKind.Object => "GetOrAddObjectTypeBuilder",
-                BuilderKind.Scalar => "GetOrAddScalarTypeBuilder",
-                _ => throw new InvalidOperationException()
-            };
-            var method = typeof(ApiSchemaBuilderContext).GetMethod(methodName, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            this.Builder1 = method!.Invoke(context, [this.ClrType!]);
-            this.Builder2 = method.Invoke(context, [this.ClrType!]);
+            var method = typeof(ApiSchemaBuilderContext).GetMethod(this.MethodName, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+
+            this.Builder1 = method!.Invoke(context, [this.ClrType]);
+            this.Builder2 = method!.Invoke(context, [this.ClrType]);
         }
 
         protected override void Assert()
         {
+            this.Builder1.Should().NotBeNull();
+            this.Builder2.Should().NotBeNull();
+
             ReferenceEquals(this.Builder1, this.Builder2).Should().BeTrue();
         }
+        #endregion
     }
+    #endregion
 
     public static TheoryDataRow<IXUnitTest>[] GetOrAddTheoryData =>
     [
         new GetOrAddTest
         {
             Name = "Scalar builder cached",
-            Kind = BuilderKind.Scalar,
+            MethodName = nameof(ApiSchemaBuilderContext.GetOrAddScalarTypeBuilder),
             ClrType = typeof(int)
         },
         new GetOrAddTest
         {
             Name = "Enum builder cached",
-            Kind = BuilderKind.Enum,
+            MethodName = nameof(ApiSchemaBuilderContext.GetOrAddEnumTypeBuilder),
             ClrType = typeof(OrderStatus)
         },
         new GetOrAddTest
         {
             Name = "Object builder cached",
-            Kind = BuilderKind.Object,
-            ClrType = typeof(Parent)
+            MethodName = nameof(ApiSchemaBuilderContext.GetOrAddObjectTypeBuilder),
+            ClrType = typeof(Order)
         }
     ];
 
